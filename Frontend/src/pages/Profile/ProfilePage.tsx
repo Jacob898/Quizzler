d39990065738e58504b8e81b1a2db72905930157
Jacob898 <jstach898@gmail.com>
2025-01-12 17:26:06 +0100

    Further work on ProfilePage

--- Frontend/src/pages/Profile/ProfilePage.tsx
+++ Frontend/src/pages/Profile/ProfilePage.tsx
@@ -13,6 +13,29 @@ const dummyUser: userProfile = {
     profilePictureUrl: "https://fwcdn.pl/fph/48/00/754800/1151700_2.8.jpg",
 }
 
+interface quiz {
+    quizTitle: string;
+    quizImageUrl: string;
+    quizResult: string;
+    quizRating: number;
+
+}
+
+const usersQuizzes = [
+    {
+        quizTitle: "quiz 1",
+        quizImageUrl: "https://fakeimg.pl/200x100",
+        quizResult: "odpowiedź 1",
+        quizRating: 5
+    },
+    {
+        quizTitle: "quiz 2",
+        quizImageUrl: "https://fakeimg.pl/200x100",
+        quizResult: "odpowiedź 3",
+        quizRating: 3
+    }
+];
+
 
 
 
@@ -25,10 +48,49 @@ const ProfilePage = () => {
                 <img src={dummyUser.profilePictureUrl} alt={"profilePictureUrl"}
                      style={{width: "10vw", borderRadius: "50%" }}/>
 
-                <h1 style={{
+                <p style={{
                     padding: "20px",
-                    fontSize: "4vh"
-                }}>{dummyUser.username}</h1>
+                    fontSize: "8vh"
+                }}>{dummyUser.username}</p>
+
+                <p style={{
+                    padding: "20px",
+                    fontSize: "4vh",
+                }}>Historia Quizów</p>
+
+                <div>
+                    {usersQuizzes.map((quiz,index) => (
+                        <div key={index} style={{
+                            display: "flex",
+                            flexDirection: "row",
+                            justifyContent: "center",
+                            margin: "20px",
+                        }}>
+
+                            <div style={{
+                                padding: "20px",
+                            }}>
+                                <img src={quiz.quizImageUrl}></img>
+                            </div>
+
+                            <div style={{
+                                padding: "20px",
+                                display: "flex",
+                                alignItems: "center",
+                                flexDirection: "column",
+                            }}>
+                                {quiz.quizTitle}
+                                {quiz.quizResult}
+                                {quiz.quizRating}
+
+                            </div>
+
+                        </div>
+                    ))}
+                </div>
+
+
+
             </div>
 
             <PageFooter/>
