ac9a631534a6d63ca1af0e992b0fd4305793bff0
Agnieszka <agadabek.sa@gmail.com>
2025-01-29 22:57:29 +0100

    style style style

--- Frontend/src/pages/LoginAndRegister/LoginPage.tsx
+++ Frontend/src/pages/LoginAndRegister/LoginPage.tsx
@@ -1,94 +1,195 @@
-import {Button, Form, Input, Layout} from "antd";
-
-import styles from './LoginPage.module.css'
-import {Link, useNavigate} from "react-router-dom";
-import {LeftOutlined} from "@ant-design/icons";
+import { Button, Form, Input, Layout } from "antd";
+import styles from "./LoginPage.module.css";
+import { useNavigate } from "react-router-dom";
+import { LeftOutlined } from "@ant-design/icons";
 
 const LoginPage = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const handleClickToHomePage = () => {
-        navigate("/");
-    }
+  const handleClickToHomePage = () => {
+    navigate("/");
+  };
 
-    const handleLogin = async (values) => {
-        try {
-            const response = await fetch("https://quizzler-backend-1.onrender.com/api/auth/login", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({
-                    email: values.email,
-                    password: values.password,
-                }),
-            });
-
-            if (response.ok) {
-                console.log("User logged in successfully ");
-                const siteResponse = await response.json();
-                localStorage.setItem("token", siteResponse.accessToken);
-                localStorage.setItem("refreshToken", siteResponse.refreshToken);
-                localStorage.setItem("userID", siteResponse.user.id);
-                localStorage.setItem("isLoggedIn", "true");
-                navigate("/");
-            } else {
-                const error = await response.json();
-                console.error("Login failed: ", error );
-                alert("Logowanie nie powiodło się");
-            }
-        } catch (error) {
-            console.error("error occured while logging in: ", error);
-            alert("Logowanie nie powiodło się");
+  const handleLogin = async (values: { email: string; password: string }) => {
+    try {
+      const response = await fetch(
+        "https://quizzler-backend-1.onrender.com/api/auth/login",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            email: values.email,
+            password: values.password,
+          }),
         }
+      );
+
+      if (response.ok) {
+        const siteResponse = await response.json();
+        localStorage.setItem("token", siteResponse.accessToken);
+        localStorage.setItem("refreshToken", siteResponse.refreshToken);
+        localStorage.setItem("userID", siteResponse.user.id);
+        localStorage.setItem("isLoggedIn", "true");
+        navigate("/");
+      } else {
+        alert("Logowanie nie powiodło się");
+      }
+    } catch (error) {
+      alert("Logowanie nie powiodło się");
     }
+  };
 
-    return (
-    <Layout>
-    <div className={styles.loginPage} >
-        <div className={styles.logoSection}>
-            <LeftOutlined className={styles.routeBackIcon} onClick={handleClickToHomePage} />
-            <div className={styles.logo}>Logo-placeholder</div>
-        </div>
+  return (
+    <Layout className={styles.loginPage}>
+      <LeftOutlined
+        className={styles.routeBackIcon}
+        onClick={handleClickToHomePage}
+      />
+      <div className={styles.loginContainer}>
+        <h1 className={styles.loginHeader}>Zaloguj się</h1>
+        <Form name="login-form" layout="vertical" onFinish={handleLogin}>
+          <Form.Item
+            label={<span className={styles.inputLabel}>Nazwa Użytkownika</span>}
+            name="email"
+            rules={[
+              { required: true, message: "Nazwa użytkownika jest wymagana" },
+            ]}
+          >
+            <Input
+              className={styles.inputField}
+              placeholder="Wpisz nazwę użytkownika"
+            />
+          </Form.Item>
 
-        <div className={styles.loginContainer}>
-            <h1 className={styles.loginHeader}>Zaloguj się</h1>
-            <Form
-                name="login-form"
-                layout="vertical"
-                onFinish={handleLogin}
+          <Form.Item
+            label={<span className={styles.inputLabel}>Hasło</span>}
+            name="password"
+            rules={[{ required: true, message: "Hasło jest wymagane" }]}
+          >
+            <Input.Password
+              className={styles.inputField}
+              placeholder="Wpisz hasło"
+            />
+          </Form.Item>
+
+          <Form.Item>
+            <Button
+              type="primary"
+              htmlType="submit"
+              className={styles.loginButton}
             >
-                <Form.Item label="Nazwa Użytkownika"
-                           name="email"
-                           rules={[{required: true, message:"Nazwa użytkownika jest wymagana"}]}
-                >
-                    <Input />
-                </Form.Item>
+              Zaloguj się
+            </Button>
+          </Form.Item>
+        </Form>
 
-                <Form.Item label="Hasło"
-                    name="password"
-                    rules={[{required: true, message:"Hasło jest wymagane"}]}
-                >
-                    <Input.Password />
-                </Form.Item>
-
-                <Form.Item>
-                    <Button type="primary" htmlType="submit" className={styles.loginButton}>
-                        Zaloguj się
-                    </Button>
-                </Form.Item>
-
-            </Form>
-
-                <Button type="primary"  className={styles.registerButton}>
-                    <Link to="/register">
-                        Załóż nowe konto
-                    </Link>
-                </Button>
-        </div>
-    </div>
+        <Button
+          type="primary"
+          className={styles.registerButton}
+          onClick={() => navigate("/register")}
+        >
+          Załóż nowe konto
+        </Button>
+      </div>
     </Layout>
-    )
-}
+  );
+};
 
 export default LoginPage;
+
+// import {Button, Form, Input, Layout} from "antd";
+
+// import styles from './LoginPage.module.css'
+// import {Link, useNavigate} from "react-router-dom";
+// import {LeftOutlined} from "@ant-design/icons";
+
+// const LoginPage = () => {
+//     const navigate = useNavigate();
+
+//     const handleClickToHomePage = () => {
+//         navigate("/");
+//     }
+
+//     const handleLogin = async (values) => {
+//         try {
+//             const response = await fetch("https://quizzler-backend-1.onrender.com/api/auth/login", {
+//                 method: "POST",
+//                 headers: {
+//                     "Content-Type": "application/json",
+//                 },
+//                 body: JSON.stringify({
+//                     email: values.email,
+//                     password: values.password,
+//                 }),
+//             });
+
+//             if (response.ok) {
+//                 console.log("User logged in successfully ");
+//                 const siteResponse = await response.json();
+//                 localStorage.setItem("token", siteResponse.accessToken);
+//                 localStorage.setItem("refreshToken", siteResponse.refreshToken);
+//                 localStorage.setItem("userID", siteResponse.user.id);
+//                 localStorage.setItem("isLoggedIn", "true");
+//                 navigate("/");
+//             } else {
+//                 const error = await response.json();
+//                 console.error("Login failed: ", error );
+//                 alert("Logowanie nie powiodło się");
+//             }
+//         } catch (error) {
+//             console.error("error occured while logging in: ", error);
+//             alert("Logowanie nie powiodło się");
+//         }
+//     }
+
+//     return (
+//     <Layout>
+//     <div className={styles.loginPage} >
+//         <div className={styles.logoSection}>
+//             <LeftOutlined className={styles.routeBackIcon} onClick={handleClickToHomePage} />
+//             <div className={styles.logo}>Logo-placeholder</div>
+//         </div>
+
+//         <div className={styles.loginContainer}>
+//             <h1 className={styles.loginHeader}>Zaloguj się</h1>
+//             <Form
+//                 name="login-form"
+//                 layout="vertical"
+//                 onFinish={handleLogin}
+//             >
+//                 <Form.Item label="Nazwa Użytkownika"
+//                            name="email"
+//                            rules={[{required: true, message:"Nazwa użytkownika jest wymagana"}]}
+//                 >
+//                     <Input />
+//                 </Form.Item>
+
+//                 <Form.Item label="Hasło"
+//                     name="password"
+//                     rules={[{required: true, message:"Hasło jest wymagane"}]}
+//                 >
+//                     <Input.Password />
+//                 </Form.Item>
+
+//                 <Form.Item>
+//                     <Button type="primary" htmlType="submit" className={styles.loginButton}>
+//                         Zaloguj się
+//                     </Button>
+//                 </Form.Item>
+
+//             </Form>
+
+//                 <Button type="primary"  className={styles.registerButton}>
+//                     <Link to="/register">
+//                         Załóż nowe konto
+//                     </Link>
+//                 </Button>
+//         </div>
+//     </div>
+//     </Layout>
+//     )
+// }
+
+// export default LoginPage;
--- Frontend/src/pages/LoginAndRegister/RegisterPage.tsx
+++ Frontend/src/pages/LoginAndRegister/RegisterPage.tsx
@@ -1,108 +1,234 @@
-import {Button, Form, Input, Layout} from "antd";
-
-import styles from './LoginPage.module.css'
-import {useNavigate} from "react-router-dom";
-import {LeftOutlined} from "@ant-design/icons";
+import { Button, Form, Input, Layout } from "antd";
+import styles from "./LoginPage.module.css";
+import { useNavigate } from "react-router-dom";
+import { LeftOutlined } from "@ant-design/icons";
 
 const RegisterPage = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const handleClickToHomePage = () => {
-        navigate("/");
+  const handleClickToHomePage = () => {
+    navigate("/");
+  };
+
+  const handleRegister = async (values: {
+    email: string;
+    password: string;
+    confirmPassword: string;
+  }) => {
+    if (values.password !== values.confirmPassword) {
+      alert("Hasła się nie zgadzają!");
+      return;
     }
 
-    const handleRegister = async (values) => {
-        try {
-            const response = await fetch("https://quizzler-backend-1.onrender.com/api/auth/register", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({
-                    email: values.email,
-                    password: values.password,
-                }),
-            });
-
-            if (response.ok) {
-                console.log("User registered successfully ");
-                const siteResponse = await response.json();
-                localStorage.setItem("token", siteResponse.accessToken);
-                localStorage.setItem("refreshToken", siteResponse.refreshToken);
-                localStorage.setItem("userID", siteResponse.user.id);
-                localStorage.setItem("isLoggedIn", "true");
-                navigate("/");
-            } else {
-                const error = await response.json();
-                console.error("Registration failed: ", error );
-                alert("Rejestracja nie powiodła się");
-            }
-        } catch (error) {
-            console.error("error occured while registering: ", error);
-            alert("Rejestracja nie powiodła się");
+    try {
+      const response = await fetch(
+        "https://quizzler-backend-1.onrender.com/api/auth/register",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            email: values.email,
+            password: values.password,
+          }),
         }
+      );
+
+      if (response.ok) {
+        const siteResponse = await response.json();
+        localStorage.setItem("token", siteResponse.accessToken);
+        localStorage.setItem("refreshToken", siteResponse.refreshToken);
+        localStorage.setItem("userID", siteResponse.user.id);
+        localStorage.setItem("isLoggedIn", "true");
+        navigate("/");
+      } else {
+        alert("Rejestracja nie powiodła się");
+      }
+    } catch (error) {
+      alert("Rejestracja nie powiodła się");
     }
+  };
 
-    return (
-        <Layout>
-            <div className={styles.loginPage} >
-                <div className={styles.logoSection}>
-                    <LeftOutlined className={styles.routeBackIcon} onClick={handleClickToHomePage} />
-                    <div className={styles.logo}>Logo-placeholder</div>
-                </div>
+  return (
+    <Layout className={styles.loginPage}>
+      <LeftOutlined
+        className={styles.routeBackIcon}
+        onClick={handleClickToHomePage}
+      />
+      <div className={styles.loginContainer}>
+        <h1 className={styles.loginHeader}>Zarejestruj się</h1>
+        <Form name="register-form" layout="vertical" onFinish={handleRegister}>
+          <Form.Item
+            label={<span className={styles.inputLabel}>Nazwa Użytkownika</span>}
+            name="email"
+            rules={[
+              { required: true, message: "Nazwa użytkownika jest wymagana" },
+            ]}
+          >
+            <Input
+              className={styles.inputField}
+              placeholder="Wpisz nazwę użytkownika"
+            />
+          </Form.Item>
 
-                <div className={styles.loginContainer}>
-                    <h1 className={styles.loginHeader}>Zarejestruj się</h1>
-                    <Form
-                        name="login-form"
-                        layout="vertical"
-                        onFinish={handleRegister}
-                    >
-                        <Form.Item label="Nazwa Użytkownika"
-                                   name="email"
-                                   rules={[{required: true, message:"Nazwa użytkownika jest wymagana"}]}
-                        >
-                            <Input />
-                        </Form.Item>
+          <Form.Item
+            label={<span className={styles.inputLabel}>Hasło</span>}
+            name="password"
+            rules={[{ required: true, message: "Hasło jest wymagane" }]}
+          >
+            <Input.Password
+              className={styles.inputField}
+              placeholder="Wpisz hasło"
+            />
+          </Form.Item>
 
-                        <Form.Item label="Hasło"
-                                   name="password"
-                                   rules={[{required: true, message:"Hasło jest wymagane"}]}
-                        >
-                            <Input.Password />
-                        </Form.Item>
+          <Form.Item
+            label={<span className={styles.inputLabel}>Powtórz Hasło</span>}
+            name="confirmPassword"
+            dependencies={["password"]}
+            rules={[
+              { required: true, message: "Powtórzenie hasła jest wymagane" },
+              ({ getFieldValue }) => ({
+                validator(_, value) {
+                  if (!value || getFieldValue("password") === value) {
+                    return Promise.resolve();
+                  }
+                  return Promise.reject(
+                    new Error("Wpisane hasła się nie zgadzają")
+                  );
+                },
+              }),
+            ]}
+          >
+            <Input.Password
+              className={styles.inputField}
+              placeholder="Powtórz hasło"
+            />
+          </Form.Item>
 
-                        <Form.Item label="Powtórz Hasło"
-                                   name = "confirm password"
-                                   dependencies={['password']}
-                                   rules ={[
-                                       {
-                                           required: true,
-                                           message: "Powtórzenie hasła jest wymagane",
-                                       },
-                                       ({ getFieldValue }) => ({
-                                           validator(_, value) {
-                                               if (!value || getFieldValue('password') === value) {
-                                                   return Promise.resolve();
-                                               }
-                                               return Promise.reject(new Error("Wpisane hasła się nie zgadzają"));
-                                           }
-                                       })
-                                   ]}
-                        >
-                            <Input.Password />
-                        </Form.Item>
-
-                        <Form.Item>
-                            <Button type="primary" htmlType="submit" className={styles.loginButton}>
-                                Zarejestruj się
-                            </Button>
-                        </Form.Item>
-                    </Form>
-                </div>
-            </div>
-        </Layout>
-    )
-}
+          <Form.Item>
+            <Button
+              type="primary"
+              htmlType="submit"
+              className={styles.loginButton}
+            >
+              Zarejestruj się
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>
+    </Layout>
+  );
+};
 
 export default RegisterPage;
+
+// import {Button, Form, Input, Layout} from "antd";
+
+// import styles from './LoginPage.module.css'
+// import {useNavigate} from "react-router-dom";
+// import {LeftOutlined} from "@ant-design/icons";
+
+// const RegisterPage = () => {
+//     const navigate = useNavigate();
+
+//     const handleClickToHomePage = () => {
+//         navigate("/");
+//     }
+
+//     const handleRegister = async (values) => {
+//         try {
+//             const response = await fetch("https://quizzler-backend-1.onrender.com/api/auth/register", {
+//                 method: "POST",
+//                 headers: {
+//                     "Content-Type": "application/json",
+//                 },
+//                 body: JSON.stringify({
+//                     email: values.email,
+//                     password: values.password,
+//                 }),
+//             });
+
+//             if (response.ok) {
+//                 console.log("User registered successfully ");
+//                 const siteResponse = await response.json();
+//                 localStorage.setItem("token", siteResponse.accessToken);
+//                 localStorage.setItem("refreshToken", siteResponse.refreshToken);
+//                 localStorage.setItem("userID", siteResponse.user.id);
+//                 localStorage.setItem("isLoggedIn", "true");
+//                 navigate("/");
+//             } else {
+//                 const error = await response.json();
+//                 console.error("Registration failed: ", error );
+//                 alert("Rejestracja nie powiodła się");
+//             }
+//         } catch (error) {
+//             console.error("error occured while registering: ", error);
+//             alert("Rejestracja nie powiodła się");
+//         }
+//     }
+
+//     return (
+//         <Layout>
+//             <div className={styles.loginPage} >
+//                 <div className={styles.logoSection}>
+//                     <LeftOutlined className={styles.routeBackIcon} onClick={handleClickToHomePage} />
+//                     <div className={styles.logo}>Logo-placeholder</div>
+//                 </div>
+
+//                 <div className={styles.loginContainer}>
+//                     <h1 className={styles.loginHeader}>Zarejestruj się</h1>
+//                     <Form
+//                         name="login-form"
+//                         layout="vertical"
+//                         onFinish={handleRegister}
+//                     >
+//                         <Form.Item label="Nazwa Użytkownika"
+//                                    name="email"
+//                                    rules={[{required: true, message:"Nazwa użytkownika jest wymagana"}]}
+//                         >
+//                             <Input />
+//                         </Form.Item>
+
+//                         <Form.Item label="Hasło"
+//                                    name="password"
+//                                    rules={[{required: true, message:"Hasło jest wymagane"}]}
+//                         >
+//                             <Input.Password />
+//                         </Form.Item>
+
+//                         <Form.Item label="Powtórz Hasło"
+//                                    name = "confirm password"
+//                                    dependencies={['password']}
+//                                    rules ={[
+//                                        {
+//                                            required: true,
+//                                            message: "Powtórzenie hasła jest wymagane",
+//                                        },
+//                                        ({ getFieldValue }) => ({
+//                                            validator(_, value) {
+//                                                if (!value || getFieldValue('password') === value) {
+//                                                    return Promise.resolve();
+//                                                }
+//                                                return Promise.reject(new Error("Wpisane hasła się nie zgadzają"));
+//                                            }
+//                                        })
+//                                    ]}
+//                         >
+//                             <Input.Password />
+//                         </Form.Item>
+
+//                         <Form.Item>
+//                             <Button type="primary" htmlType="submit" className={styles.loginButton}>
+//                                 Zarejestruj się
+//                             </Button>
+//                         </Form.Item>
+//                     </Form>
+//                 </div>
+//             </div>
+//         </Layout>
+//     )
+// }
+
+// export default RegisterPage;
